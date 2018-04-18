3de433ef3af09dbe416df543f6406e35480a7f37
-k <slowdive@me.com>
2018-04-17 22:27:59 -0400

    add swap

--- p2pool.deploy.sh
+++ p2pool.deploy.sh
@@ -65,6 +65,14 @@ sudo ufw logging on
 sudo ufw disable
 sudo ufw enable
 
+# Enable 2G swap
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo swapon --show
+
 #
 # Get latest p2pool-AXE
 #
