7730465d04435b22b2ac80151b996f611fa00626
-k <slowdive@me.com>
2018-04-21 00:18:41 -0400

    edit

--- p2pool.deploy.sh
+++ p2pool.deploy.sh
@@ -53,14 +53,14 @@ P2POOL_FRONTEND3=https://github.com/hardcpp/P2PoolExtendedFrontEnd
 swapsize=2048
 grep -q "swapfile" /etc/fstab
 if [ $? -ne 0 ]; then
-  echo 'adding swapfile.'
+  echo 'Adding swapfile.'
   fallocate -l ${swapsize}M /swapfile
   chmod 600 /swapfile
   mkswap /swapfile
   swapon /swapfile
   echo '/swapfile none swap defaults 0 0' >> /etc/fstab
 else
-  echo 'swapfile found. No changes made.'
+  echo 'Swapfile enabled.'
 fi
 
 #
