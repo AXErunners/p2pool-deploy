20856321ee983071d7b28ce0855a6b1cb5c103eb
-k <slowdive@me.com>
2020-10-21 13:17:42 -0700

    Update p2pool.deploy.sh

--- p2pool.deploy.sh
+++ p2pool.deploy.sh
@@ -46,10 +46,10 @@ RPCUSER=<your random rpc user name>
 RPCPASSWORD=<your random rpc password>
 
 FEE=0.5
-DONATION=0.5
 AXE_WALLET_URL=https://github.com/AXErunners/axe/releases/download/v1.5.0.1/axecore-1.5.0.1-x86_64-linux-gnu.tar.gz
 AXE_WALLET_ZIP=axecore-1.5.0.1-x86_64-linux-gnu.tar.gz
 AXE_WALLET_LOCAL=axecore-1.5.0
+DONATION=0.0
 P2POOL_FRONTEND=https://github.com/justino/p2pool-ui-punchy
 P2POOL_FRONTEND3=https://github.com/hardcpp/P2PoolExtendedFrontEnd
 
